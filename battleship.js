647976e6d8eda2bea48e48ada97b1d6b21e24d27
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2024-01-24 11:15:17 -0600

    updated js file with view methods and initial test

--- battleship.js
+++ battleship.js
@@ -1,34 +1,21 @@
-var randomLoc = Math.floor(Math.random() * 5);
+var view = {
+  displayMessage: function (msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
 
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-
-var guess;
-var hits = 0;
-var guesses = 0;
-
-var isSunk = false;
-
-while (isSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-  if (guess < 0 || guess > 6) {
-    alert("please enter a valid cell number!");
-  } else {
-    guesses += 1;
-    if (guess == location1 || guess == location2 || guess == location3) {
-      alert("HIT!");
-      hits += 1;
-      if (hits == 3) {
-        isSunk = true;
-        alert("You sank my battleship!");
-      }
-    } else {
-      alert("MISS!");
-    }
-  }
-}
-var stats = `You took ${guesses} guesses to sink the battleship, whiich means your shooting accuracy was ${
-  3 / guesses
-}`;
-alert(stats);
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("26");
+view.displayMessage("Tap tap, is this thing on?");
